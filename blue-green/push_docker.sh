27fd410538855a4e1e05b54b8b988d66d923ac70
Gharabawi <mada_meanyaz@yahoo.com>
2022-06-12 14:38:45 +0200

    2nd push

--- blue-green/push_docker.sh
+++ blue-green/push_docker.sh
@@ -6,9 +6,6 @@
 # Create dockerpath
 dockerpath=mada96/capstone_project
 # Step2:
-# Docker login
-docker login --username $DOCKER_USER --password $DOCKER_PASSWORD
-# Step3:
 # Docker push
 docker image tag $dockerpath:0.0 $dockerpath:$1
 docker image push $dockerpath:$1
